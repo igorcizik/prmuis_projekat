f4b4dc970937be43bb0e7581ef3dced538e03e3f
nemanjavuksa <vuksanemanja1289@gmail.com>
2026-02-02 19:32:53 +0100

    dodata klasa korisnik

---
+++ Korisnik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Korisnik
+{
+    public string Ime { get; set; }
+    public string Prezime { get; set; }
+    public string KorisnickoIme { get; set; }
+    public string Lozinka { get; set; }
+    public bool StatusPrijave { get; set; }
+    public int Port { get; set; }
+
+    public Korisnik() { }
+
+    public Korisnik(string ime, string prezime, string korisnickoIme,
+                    string lozinka, bool statusPrijave, int port)
+    {
+        Ime = ime;
+        Prezime = prezime;
+        KorisnickoIme = korisnickoIme;
+        Lozinka = lozinka;
+        StatusPrijave = statusPrijave;
+        Port = port;
+    }
+}
