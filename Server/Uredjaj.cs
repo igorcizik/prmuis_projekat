20a874681fb87da771eda41542d15efd863e933c
igor.cizik <igor.cizik2004@gmail.com>
2026-02-02 23:00:32 +0100

    klasa uredjaj

---
+++ Server/Uredjaj.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Uredjaj
+    {
+
+        public string ImeUredjaja { get; set; }
+        public int Port { get; set; }
+        public Dictionary<string, int> Funkcije { get; set; }
+
+
+        public Uredjaj() { this.Funkcije = new Dictionary<string, int>(); }
+
+        public Uredjaj(string imeUredjaja, int port)
+        {
+            this.Funkcije = new Dictionary<string, int>();
+            this.ImeUredjaja = imeUredjaja;
+            this.Port = port;
+            
+        }
+
+        public void dodajFunkciju(string funkcija)
+        {
+            if (!Funkcije.ContainsKey(funkcija))
+            {
+                Funkcije[funkcija] = 0;
+            }
+        }
+     
+
+        }
+    }      
+
---
+++ Svetla/Svetla.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Svetla
+{
+    class Svetla
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
